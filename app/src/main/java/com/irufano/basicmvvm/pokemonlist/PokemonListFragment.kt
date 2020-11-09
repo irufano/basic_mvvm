e455a3d50c9d2a8c25f92629eb213b0b71f05f22
M. Irfan Hidayat <irfanhidayatms@gmail.com>
2020-11-09 08:46:10 +0700

    add fragment res

---
+++ app/src/main/java/com/irufano/basicmvvm/pokemonlist/PokemonListFragment.kt
@@ -0,0 +1,15 @@
+package com.irufano.basicmvvm.pokemonlist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_pokemon_list.*
+
+class PokemonListFragment : Fragment() {
+
+}
